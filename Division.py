3b22cefb7b7fff4be80cf073ad9b217e73509380
vaibhav <vaibhavnalla15@gmail.com>
2025-09-30 13:50:13 +0530

    Division

---
+++ Division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input("Enter a num:- "))
+    b = int(input("Enter a num:- "))
+    
+    integer_division = a // b
+    float_division = a / b
+
+    print(integer_division)
+    print(float_division)
