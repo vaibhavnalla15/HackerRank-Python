0ebfdee3e7567135bd34b83494f45f20ed53d8fe
vaibhav <vaibhavnalla15@gmail.com>
2025-09-30 19:24:19 +0530

    Print Function

---
+++ Print Function.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input("Enter a number:- "))
+
+for i in range(1,n+1):
+    print(i, end="") # end=" " makes print stay on the same line with a space instead of jumping to a new line.
